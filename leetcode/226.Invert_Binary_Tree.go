104657e67ccee5ca95118e0b9c4736078e94538b
kaiiak <aNxFi37X@outlook.com>
2021-06-11 17:38:15 +0800

    https://leetcode.com/problems/invert-binary-tree/

---
+++ leetcode/226.Invert_Binary_Tree.go
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/invert-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func invertTree(root *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+
+    root.Left, root.Right = root.Right, root.Left
+    invertTree(root.Left)
+    invertTree(root.Right)
+
+    return root
+}
