afa56c89d3c35460774087d0e0626ef9864f0658
kaiiak <aNxFi37X@outlook.com>
2024-02-23 21:53:28 +0800

    https://leetcode.cn/problems/kth-largest-sum-in-a-binary-tree/

---
+++ leetcode/2583.kth-largest-sum-in-a-binary-tree.go
@@ -0,0 +1,34 @@
+// https://leetcode.cn/problems/kth-largest-sum-in-a-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthLargestLevelSum(root *TreeNode, k int) int64 {
+	var sums []int64
+	dfs(root, 0, &sums)
+
+	sort.Slice(sums, func(i, j int) bool {
+		return sums[i] > sums[j]
+	})
+	if k > len(sums) {
+		return -1
+	}
+	return sums[k-1]
+}
+
+func dfs(root *TreeNode, level int, sum *[]int64) {
+	if root == nil {
+		return
+	}
+	if len(*sum) <= level {
+		*sum = append(*sum, 0)
+	}
+	(*sum)[level] += int64(root.Val)
+	dfs(root.Left, level+1, sum)
+	dfs(root.Right, level+1, sum)
+}
