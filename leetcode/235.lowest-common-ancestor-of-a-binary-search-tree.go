409e62869f6068ca34412bd82adc147cfe6c8f0f
kaiiak <aNxFi37X@outlook.com>
2024-02-25 21:49:35 +0800

    https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/

---
+++ leetcode/235.lowest-common-ancestor-of-a-binary-search-tree.go
@@ -0,0 +1,20 @@
+// https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+	if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	return root
+ }
