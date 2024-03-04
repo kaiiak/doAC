3ca5d2d9bb39177551d17a7ad73e9cf3aee410d5
kaiiak <aNxFi37X@outlook.com>
2024-03-04 20:55:07 +0800

    使用两个栈实现先入先出队列

--- leetcode/232.implement-queue-using-stacks.go
+++ leetcode/232.implement-queue-using-stacks.go
@@ -1,44 +1,70 @@
 // https://leetcode.cn/problems/implement-queue-using-stacks/
 
-type MyQueue struct {
+// 请你仅使用两个栈实现先入先出队列。
+
+type MyStack struct {
 	s []int
 }
 
+func (s *MyStack) Pop() int {
+	if len(s.s) == 0 {
+		return 0
+	}
+	val := s.s[len(s.s)-1]
+	s.s = s.s[:len(s.s)-1]
+	return val
+}
+
+func NewStack() *MyStack {
+	return &MyStack{s: make([]int, 0)}
+}
+
+func (s *MyStack) Push(val int) {
+	s.s = append(s.s, val)
+}
+
+func (s *MyStack) Len() int {
+	return len(s.s)
+}
+
+type MyQueue struct {
+	in, out *MyStack
+}
+
 func Constructor() MyQueue {
-	return MyQueue{s: make([]int, 0)}
+	return MyQueue{in: NewStack(), out: NewStack()}
+}
+
+func (this *MyQueue) in2out() {
+	if this.out.Len() == 0 {
+		for this.in.Len() > 0 {
+			this.out.Push(this.in.Pop())
+		}
+	}
 }
 
 // 将元素 x 推到队列的末尾
 func (this *MyQueue) Push(x int) {
-	this.s = append(this.s, x)
+	this.in.Push(x)
 }
 
 // 从队列的开头移除并返回元素
 func (this *MyQueue) Pop() (val int) {
-	if len(this.s) == 0 {
-		return 0
-	}
-	if len(this.s) == 1 {
-		val = this.s[0]
-		this.s = this.s[0:0]
-		return
-
-	}
-	val = this.s[0]
-	this.s = this.s[1:len(this.s)]
-	return
+	this.in2out()
+	return this.out.Pop()
 }
 
 // 返回开头的元素
 func (this *MyQueue) Peek() int {
-	if len(this.s) > 0 {
-		return this.s[0]
+	this.in2out()
+	if this.out.Len() > 0 {
+		return this.out.s[this.out.Len()-1]
 	}
 	return 0
 }
 
 func (this *MyQueue) Empty() bool {
-	return len(this.s) == 0
+	return this.in.Len() == 0 && this.out.Len() == 0
 }
 
 /**
